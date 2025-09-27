5a7f11cf1a644667ba6c4fdc6567fa62d30dc04a
huluhuluwawa <guoyuxin822@gmail.com>
2025-09-28 01:55:28 +0800

    Update style.js

--- themes/next/style.js
+++ themes/next/style.js
@@ -5,31 +5,55 @@
  * @returns
  */
 const Style = () => {
-  return (
-    <style jsx global>{`
-      // 底色
-      body {
-        background-color: #eeedee;
-      }
-      .dark body {
-        background-color: black;
-      }
+  return (
+    <style jsx global>{`
+      // 底色和全局字体 (已更新为参考网站的字体堆栈)
+      body {
+        background-color: #eeedee;
+        /* 設置全局字體：優先使用現代系統無襯線和思源黑體 */
+        font-family:
+          Source Sans Pro,
+          Roboto,
+          Helvetica,
+          Helvetica Neue,
+          PingFang SC,
+          PingFang HK,
+          PingFang TC,
+          Source Han Sans SC,
+          Source Han Sans TC,
+          sans-serif;
+      }
+      .dark body {
+        background-color: black;
+        /* 設置夜間模式字體 */
+        font-family:
+          Source Sans Pro,
+          Roboto,
+          Helvetica,
+          Helvetica Neue,
+          PingFang SC,
+          PingFang HK,
+          PingFang TC,
+          Source Han Sans SC,
+          Source Han Sans TC,
+          sans-serif;
+      }
 
-      // 菜单下划线动画
-      #theme-next .menu-link {
-        text-decoration: none;
-        background-image: linear-gradient(#4e80ee, #4e80ee);
-        background-repeat: no-repeat;
-        background-position: bottom center;
-        background-size: 0 2px;
-        transition: background-size 100ms ease-in-out;
-      }
-      #theme-next .menu-link:hover {
-        background-size: 100% 2px;
-        color: #4e80ee;
-      }
-    `}</style>
-  )
+      // 菜单下划线动画
+      #theme-next .menu-link {
+        text-decoration: none;
+        background-image: linear-gradient(#4e80ee, #4e80ee);
+        background-repeat: no-repeat;
+        background-position: bottom center;
+        background-size: 0 2px;
+        transition: background-size 100ms ease-in-out;
+      }
+      #theme-next .menu-link:hover {
+        background-size: 100% 2px;
+        color: #4e80ee;
+      }
+    `}</style>
+  )
 }
 
 export { Style }
